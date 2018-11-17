ab25904330e3bfd4c777efd48f8701577e7d644d
Konstantin Đorđević <vomindoraan@gmail.com>
2018-11-17 19:47:30 +0100

    Refactor and improve build.sh script

--- workspace/TS100/build.sh
+++ workspace/TS100/build.sh
@@ -1,103 +1,18 @@
-python ../../Translation\ Editor/make_translation.py ../../Translation\ Editor
+#!/bin/bash
+TRANS_DIR="../../Translation Editor"
+
+python "$TRANS_DIR/make_translation.py" "$TRANS_DIR"
 
 make clean
 
-# TS100
-make -j16 lang=EN
-rm -rf Objects/src
-make -j16 lang=BG
-rm -rf Objects/src
-make -j16 lang=CS
-rm -rf Objects/src
-make -j16 lang=DA
-rm -rf Objects/src
-make -j16 lang=DE
-rm -rf Objects/src
-make -j16 lang=ES
-rm -rf Objects/src
-make -j16 lang=FI
-rm -rf Objects/src
-make -j16 lang=FR
-rm -rf Objects/src
-make -j16 lang=HR
-rm -rf Objects/src
-make -j16 lang=HU
-rm -rf Objects/src
-make -j16 lang=IT
-rm -rf Objects/src
-make -j16 lang=LT
-rm -rf Objects/src
-make -j16 lang=NL
-rm -rf Objects/src
-make -j16 lang=NL_BE
-rm -rf Objects/src
-make -j16 lang=NO
-rm -rf Objects/src
-make -j16 lang=PL
-rm -rf Objects/src
-make -j16 lang=PT
-rm -rf Objects/src
-make -j16 lang=RU
-rm -rf Objects/src
-make -j16 lang=SK
-rm -rf Objects/src
-make -j16 lang=SR_CYRL
-rm -rf Objects/src
-make -j16 lang=SR_LATN
-rm -rf Objects/src
-make -j16 lang=SV
-rm -rf Objects/src
-make -j16 lang=TR
-rm -rf Objects/src
-make -j16 lang=UK
-rm -rf Objects/src
+for model in "TS100" "TS80"; do
+    for f in "$TRANS_DIR"/translation_*.json; do
+        lang_json=${f#*_}     # Remove ".../translation_"
+        lang=${lang_json%.*}  # Remove ".json"
+        lang=${lang^^}        # Convert to uppercase
 
-# TS80
-make -j16 lang=EN model=TS80
-rm -rf Objects/src
-make -j16 lang=BG model=TS80
-rm -rf Objects/src
-make -j16 lang=CS model=TS80
-rm -rf Objects/src
-make -j16 lang=DA model=TS80
-rm -rf Objects/src
-make -j16 lang=DE model=TS80
-rm -rf Objects/src
-make -j16 lang=ES model=TS80
-rm -rf Objects/src
-make -j16 lang=FI model=TS80
-rm -rf Objects/src
-make -j16 lang=FR model=TS80
-rm -rf Objects/src
-make -j16 lang=HR model=TS80
-rm -rf Objects/src
-make -j16 lang=HU model=TS80
-rm -rf Objects/src
-make -j16 lang=IT model=TS80
-rm -rf Objects/src
-make -j16 lang=LT model=TS80
-rm -rf Objects/src
-make -j16 lang=NL model=TS80
-rm -rf Objects/src
-make -j16 lang=NL_BE model=TS80
-rm -rf Objects/src
-make -j16 lang=NO model=TS80
-rm -rf Objects/src
-make -j16 lang=PL model=TS80
-rm -rf Objects/src
-make -j16 lang=PT model=TS80
-rm -rf Objects/src
-make -j16 lang=RU model=TS80
-rm -rf Objects/src
-make -j16 lang=SK model=TS80
-rm -rf Objects/src
-make -j16 lang=SR_CYRL model=TS80
-rm -rf Objects/src
-make -j16 lang=SR_LATN model=TS80
-rm -rf Objects/src
-make -j16 lang=SV model=TS80
-rm -rf Objects/src
-make -j16 lang=TR model=TS80
-rm -rf Objects/src
-make -j16 lang=UK model=TS80
-rm -rf Objects/src
+        # Build firmware
+        make -j16 model="$model" lang="$lang"
+        rm -rf Objects/src
+    done
+done
